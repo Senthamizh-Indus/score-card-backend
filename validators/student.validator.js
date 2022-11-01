53bbaea8abeac63b23c2067ceeb1864996a05ff6
Senthamizh <senthamizh.s.muthurangan@indusnet.co.in>
2022-11-01 12:24:38 +0530

    validator

--- validators/student.validator.js
+++ validators/student.validator.js
@@ -2,9 +2,9 @@ const Joi = require('joi');
 
 const addStudentSchema = Joi.object().keys({
     student_name: Joi.string().required(),
-    standard: Joi.string().required(),
+    standard: Joi.number().required(),
     section: Joi.string().required(),
-    rollno: Joi.string().required(),
+    rollno: Joi.number().required(),
 });
 
 module.exports = addStudentSchema
